a36773835c8ebef8fe44bb736a49ae94d713df92
Francesco Marino <francesco.marino@cybaze.it>
2020-10-12 03:58:56 +0200

    Fix: missing plugin/dep/open class

--- src/main/kotlin/com/example/demo/DemoApplication.kt
+++ src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class DemoApplication
+open class DemoApplication
 
 fun main(args: Array<String>) {
     runApplication<DemoApplication>(*args)
